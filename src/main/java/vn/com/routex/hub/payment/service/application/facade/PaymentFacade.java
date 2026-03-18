98d0bda0489bb9c87c7640f7cd65652be8bfad2d
Bao Pham <louis.phammm@gmail.com>
2026-03-18 19:36:44 +0700

    Update database

--- src/main/java/vn/com/routex/hub/payment/service/application/facade/PaymentFacade.java
+++ src/main/java/vn/com/routex/hub/payment/service/application/facade/PaymentFacade.java
@@ -1,8 +1,6 @@
 package vn.com.routex.hub.payment.service.application.facade;
 
-import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import vn.com.routex.hub.payment.service.controller.model.payment.CheckoutRequest;
 import vn.com.routex.hub.payment.service.controller.model.payment.CheckoutResponse;
 import vn.com.routex.hub.payment.service.controller.model.payment.CreatePaymentSessionRequest;
 import vn.com.routex.hub.payment.service.controller.model.payment.CreatePaymentSessionResponse;
--- src/main/java/vn/com/routex/hub/payment/service/controller/model/payment/CheckoutResponse.java
+++ src/main/java/vn/com/routex/hub/payment/service/controller/model/payment/CheckoutResponse.java
@@ -1,12 +1,10 @@
 package vn.com.routex.hub.payment.service.controller.model.payment;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 import vn.com.routex.hub.payment.service.controller.model.base.BaseResponse;
-import vn.com.routex.hub.payment.service.domain.booking.BookingStatus;
 import vn.com.routex.hub.payment.service.domain.booking.PaymentStatus;
 
 import java.math.BigDecimal;
